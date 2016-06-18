872e67d10915bfd3bb23fe5e3603444a04e6f5f4
Darwin Corn <ndarwincorn@gmail.com>
2016-06-17 20:29:50 -0600

    Added common folder for base definitions.

---
+++ common/variables.lua
@@ -0,0 +1,12 @@
+-- common
+modkey     = "Mod4"
+altkey     = "Mod1"
+terminal   = "terminator"
+editor     = os.getenv("EDITOR") or "nano" or "vim"
+editor_cmd = terminal .. " -e " .. editor
+
+-- user defined
+browser    = "chromium"
+gui_editor = "atom"
+graphics   = "inkscape"
+mail       = "thunderbird"
--- rc.lua
+++ rc.lua
@@ -1,2 +1,59 @@
-local theme = "virtual"
+--[[
+
+     Requiem For A Theory's Awesome WM config 1.0
+     github.com/requiemforatheory/awesome-r4at
+
+--]]
+
+-- Use common for default (non-theme-specific) settings. Currently based on multicolor.
+-- Theme Declaration
+local theme = "ultra"
+local themedir = os.getenv("HOME") .. "/.config/awesome/rc.d/" .. theme
+
+-- {{{ Required libraries
+gears     = require("gears")
+awful     = require("awful")
+awful.rules     = require("awful.rules")
+                  require("awful.autofocus")
+wibox     = require("wibox")
+beautiful = require("beautiful")
+naughty   = require("naughty")
+drop      = require("scratchdrop")
+lain      = require("lain")
+-- }}}
+
+local commondir = os.getenv("HOME") .. "/.config/awesome/common/"
+
+dofile(commondir .. "error-handling.lua")
+
+-- {{{ Variable definitions
+beautiful.init(themedir .. "/theme/theme.lua")
+
+dofile(commondir .. "variables.lua")
+dofile(commondir .. "layouts.lua")
+
+-- }}}
+
+-- {{{ Wallpaper
+
+if beautiful.wallpaper then
+    for s = 1, screen.count() do
+        gears.wallpaper.maximized(beautiful.wallpaper[s], s, true)
+    end
+end
+
+-- }}}
+
+dofile(commondir .. "context-menu.lua")
+dofile(commondir .. "wibox-def.lua")
+dofile(commondir .. "wibox-layout.lua")
+dofile(commondir .. "bindings-mouse.lua")
+dofile(commondir .. "bindings-keys.lua")
+dofile(commondir .. "rules.lua")
+dofile(commondir .. "signals.lua")
+
+-- {{{Theme-Specific Settings (may overwrite defaults)
+
 dofile(os.getenv("HOME") .. "/.config/awesome/rc.d/" .. theme .. "/rc.lua")
+
+-- }}}
