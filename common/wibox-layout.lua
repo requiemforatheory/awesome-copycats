ce658c904d91cd9c3ae5a1a832a7f72c6237de61
Darwin Corn <ndarwincorn@gmail.com>
2016-11-03 05:31:14 -0600

    Updated systray to appear on all screens.

--- common/wibox-layout.lua
+++ common/wibox-layout.lua
@@ -82,7 +82,7 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the upper right
     local right_layout = wibox.layout.fixed.horizontal()
-    if s == 2 then right_layout:add(wibox.widget.systray()) end
+    right_layout:add(wibox.widget.systray()) end
     -- right_layout:add(wibox.widget.systray())
     --right_layout:add(mailicon)
     --right_layout:add(mailwidget)
